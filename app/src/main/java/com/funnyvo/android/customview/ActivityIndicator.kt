5ba08806e82785bdef8dc7d5cd1eb8b0a9a5ab33
Terril Thomas <vente9@sg.uob>
2020-07-03 09:56:58 +0800

    Update Act Indicator

---
+++ app/src/main/java/com/funnyvo/android/customview/ActivityIndicator.kt
@@ -0,0 +1,50 @@
+package uob.com.digitalbank.utils
+
+import android.content.Context
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.uob.digitalbank.playground.dialog.PgLoaderDialog
+import java.util.concurrent.atomic.AtomicBoolean
+
+class ActivityIndicator(context: Context, private val lifecycle: Lifecycle) : LifecycleObserver {
+    private val dialog = PgLoaderDialog(context).apply { isCancellable = false }
+    private var isRealHide = AtomicBoolean(true)
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    fun show() {
+        if (lifecycle.currentState.isAtLeast(Lifecycle.State.CREATED) && !dialog.isShowing) {
+            dialog.show()
+        }
+    }
+
+    fun hide() {
+        isRealHide.set(true)
+        if (dialog.isShowing) {
+            dialog.dismiss()
+        }
+    }
+
+    fun showForLifecycle() {
+        if (!dialog.isShowing && !isRealHide.get()) {
+            dialog.show()
+        }
+    }
+
+    fun hideForLifecycle() {
+        if (dialog.isShowing) {
+            isRealHide.set(false)
+            dialog.dismiss()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        hide() // https://stackoverflow.com/questions/22924825/view-not-attached-to-window-manager-crash
+
+        lifecycle.removeObserver(this)
+    }
+}
