093213449ae3f0a5265e9b6b3664a91acbef8606
Terril Thomas <vente9@sg.uob>
2020-09-03 10:28:51 +0800

    Make an old version build work

--- app/src/main/java/com/funnyvo/android/videorecording/data/VideoRecording.kt
+++ app/src/main/java/com/funnyvo/android/videorecording/data/VideoRecording.kt
@@ -8,11 +8,11 @@ data class VideoRecording(val isStarted: Boolean? = false,
                           val isCancelled: Boolean? = false,
                           val hasFailed: Boolean? = false)
 
-data class VideoFilters(@SerializedName("msg") val msg: List<RecordingFilters> = mutableListOf())
+data class VideoFilters(@SerializedName("msg") val msg: List<RecordingFilters> = emptyList())
 
 data class RecordingFilters(@SerializedName("filter_id") val filterId: String? = "",
                             @SerializedName("name") val name: String? = "",
                             @SerializedName("description") val description: String? = "",
                             @SerializedName("main_image") val main_image: String? = "",
                             @SerializedName("is_gif") val isGif: String? = "0",
-                            @SerializedName("image_url") val image_url: List<String>? = mutableListOf())
+                            @SerializedName("image_url") val image_url: List<String>? = emptyList())
